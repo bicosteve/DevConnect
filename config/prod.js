bc653008e1be1f69a6b23ec7901014fb5b689309
bicosteve <bicosteve4@gmail.com>
2021-02-16 05:51:54 +0300

    Added environment variables

--- config/prod.js
+++ config/prod.js
@@ -7,6 +7,9 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   googleCallbackUrl: process.env.GOOGLE_CALL_BACK_URL,
   redirectDomain: process.env.REDIRECT_DOMAIN,
+  cloudinaryName: process.env.CLOUDINARY_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinarySecret: process.env.CLOUDINARY_SECRET,
 };
 
 //https://serene-taiga-00188.herokuapp.com/
